f032a402781c4fa1b1ff15c5661b55beca393fee
Stanimir Stoev <stoevs@gmail.com>
2021-06-24 15:54:55 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 
 const path = require("path");
 const getRootPath = path.normalize(path.join(__dirname, "/../../"));
-const localHostConnectionString = "mongodb://localhost:27017/infshroom";
+const localHostConnectionString = "mongodb+srv://Brevering:Parol@ribame4@clusterzero.lqsso.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 
 module.exports = {
     development: {
